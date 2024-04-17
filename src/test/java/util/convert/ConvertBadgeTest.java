a1977c63f322d9a56edb3e4d20462455f8beee4f
kannstas <k.annstas@gmail.com>
2024-04-17 23:10:58 +0300

    test jenkins build 1

--- src/test/java/util/convert/ConvertBadgeTest.java
+++ src/test/java/util/convert/ConvertBadgeTest.java
@@ -4,33 +4,32 @@ import api.request.badge.CreateBadgeRequest;
 import api.response.badge.GetBadgeResponse;
 import model.Badge;
 import org.junit.jupiter.api.Test;
-import java.sql.Timestamp;
+
+import java.time.Instant;
 import java.util.Calendar;
 import java.util.UUID;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConvertBadgeTest {
     @Test
     void badgeToBadgeResponse() {
 
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
         Calendar calendar = Calendar.getInstance();
-
-        calendar.setTimeInMillis(currentTime.getTime());
         calendar.add(Calendar.YEAR, 1);
-        Timestamp plusYear = new Timestamp(calendar.getTimeInMillis());
+        Instant plusYear = calendar.toInstant();
 
         Badge badge = new Badge(
                 UUID.randomUUID(),
                 UUID.randomUUID(),
                 UUID.randomUUID(),
                 "badgeSerialNumber",
-                new Timestamp(System.currentTimeMillis()),
+                Instant.now(),
                 plusYear,
                 true
         );
 
-        GetBadgeResponse badgeResponse = ConvertBadge.badgeToBadgeResponse(badge);
+        GetBadgeResponse badgeResponse = ConvertBadge.toBadge(badge);
 
         assertEquals(badge.id(), badgeResponse.id());
         assertEquals(badge.recipientEmployeeId(), badgeResponse.recipientEmployeeId());
@@ -48,7 +47,7 @@ public class ConvertBadgeTest {
                 UUID.randomUUID(),
                 "badgeSerialNumber"
         );
-        Badge badge = ConvertBadge.badgeRequestToBadge(badgeRequest);
+        Badge badge = ConvertBadge.toBadge(badgeRequest);
 
         assertEquals(badgeRequest.recipientEmployeeId(), badge.recipientEmployeeId());
         assertEquals(badgeRequest.issuerEmployeeId(), badge.issuerEmployeeId());
